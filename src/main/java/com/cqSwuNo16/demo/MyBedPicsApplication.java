cede2c5201e6a4c5bb537467009184574c30bb3a
cloverBoy <1458200660@qq.com>
2022-09-15 02:47:04 +0800

    first to commit

---
+++ src/main/java/com/cqSwuNo16/demo/MyBedPicsApplication.java
@@ -0,0 +1,13 @@
+package com.cqSwuNo16.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBedPicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBedPicsApplication.class, args);
+	}
+
+}
